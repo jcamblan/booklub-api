8270791fc42beb8001e4d37e9a823b41d58e7191
Julien Camblan <julien@tymate.com>
2020-09-20 12:19:21 +0200

    build: order by created_at bc of uuids

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  self.implicit_order_column = :created_at
 
   def uuid
     GraphQL::Schema::UniqueWithinType.encode(self.class.name, id)

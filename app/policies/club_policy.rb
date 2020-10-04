f18058e8efe27be51c91de604249f4fef54ba53f
Julien Camblan <julien@tymate.com>
2020-10-04 15:58:14 +0200

    fix: typo

--- app/policies/club_policy.rb
+++ app/policies/club_policy.rb
@@ -15,5 +15,5 @@ class ClubPolicy < ApplicationPolicy
 
   alias_rule :create?, :list_mine?, to: :authenticated?
   alias_rule :reset_invitation_code?, :invitation_code?, to: :club_manager?
-  alias_rule :users, :show?, to: :club_member?
+  alias_rule :users?, :show?, to: :club_member?
 end

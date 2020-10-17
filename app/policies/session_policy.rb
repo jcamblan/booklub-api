df4d6642737233157f639df2df6d58080af00557
Julien Camblan <julien@tymate.com>
2020-10-17 20:54:44 +0200

    refactor: clarify session policies

--- app/policies/session_policy.rb
+++ app/policies/session_policy.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class SessionPolicy < ApplicationPolicy
-  def show?
+  def club_member?
     return false unless authenticated?
 
     user.clubs.include?(record.club)
   end
 
-  alias_rule :create?, :participate?, to: :show?
+  def participate?
+    club_member? && record.submissions.find_by(user: user).nil?
+  end
+
+  alias_rule :show?, :create?, to: :club_member?
 end

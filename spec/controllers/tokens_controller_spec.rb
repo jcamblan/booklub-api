0a830037632a552ee045835c7e29b40ef8a8363c
Julien Camblan <julien.camblan@gmail.com>
2021-05-30 17:03:27 +0200

    test: add oauth/token spec

---
+++ spec/controllers/tokens_controller_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe 'oauth2 controller', type: :request do
+  let(:application) { Fabricate(:application, confidential: false) }
+  let(:user) { Fabricate(:user) }
+
+  let(:params) do
+    {
+      username: user.email,
+      password: user.password,
+      client_id: application.uid,
+      scopes: application.scopes,
+      grant_type: 'password'
+    }
+  end
+
+  it 'returns the right token' do
+    post '/oauth/token', params: params
+
+    expect(response_body[:access_token])
+      .to eq(Doorkeeper::AccessToken.where(resource_owner_id: user.id).last&.token)
+  end
+end
